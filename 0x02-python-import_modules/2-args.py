e6716840c9d0831d55a971d522cb01de74b5562b
AbreshAlex <abrehamgebeta@gmail.com>
2022-08-26 01:43:27 +0300

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    args_len = len(sys.argv)
-    if args_len == 1:
-        print("{} arguments.".format(args_len - 1))
-    elif args_len == 2:
-        print("{} argument:".format(args_len - 1))
-        print("{}: {}".format(args_len - 1, sys.argv[1]))
-    elif args_len > 2:
-        print("{} arguments:".format(args_len - 1))
-        i = 1
-        while i < args_len:
-            print("{}: {}".format(i, sys.argv[i]))
-            i += 1
+    from sys import argv
+argc = len(argv)
+if argc < 2:
+    print("{} arguments.".format(argc - 1))
+else:
+    if argc == 2:
+        print("{} argument:".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
+    for n in range(1, argc):
+        print("{}: {}".format(n, argv[n]))
